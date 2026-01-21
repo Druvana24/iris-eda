facb636403f30e48fecf8afae716072cd6f5ff73
Druvana24 <druvanabattula24@gmail.com>
2026-01-21 14:37:04 -0500

    Update iris_eda.py

--- notebooks/src/iris_eda.py
+++ notebooks/src/iris_eda.py
@@ -9,3 +9,14 @@ df["species"] = iris.target
 df["species"] = df["species"].map(dict(zip(range(3), iris.target_names)))
 
 print(df.head()
+
+
+print("Shape:", df.shape)
+print("\nInfo:")
+print(df.info())
+
+print("\nSummary statistics:")
+print(df.describe())
+
+print("\nClass balance:")
+print(df["species"].value_counts())
